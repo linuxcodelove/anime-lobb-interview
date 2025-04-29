cd54ce59036c21d0a126e3d737a718d4274d8906
muthukumar <muthu@ardhika.com>
2025-04-29 11:45:10 +0530

    sample console added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -135,6 +135,7 @@ export default {
     ...mapGetters(["getCurrentImg", "getToken"]),
     // Format date to Day-Date-Month
     getFormattedDate() {
+      console.log("hiiiiiii");
       const date = new Date();
       const dayOptions = { weekday: "long" };
       const monthOptions = { month: "long" };
